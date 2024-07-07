b521ab0cb99d653c337735360707e48ad80da5ee
Daniel Guzman Sagredo <danguz80@outlook.com>
2024-07-07 05:27:04 -0400

    Terminando, me faltan las fechas

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,15 +1,19 @@
 const formulario = document.querySelector("#formulario")
-
 const obtenerData = async (moneda) => {
-    const data = await fetch(`https://mindicador.cl/api/${moneda}`)
-    const res = await data.json()
-    return res
-}
+    try {
+        const data = await fetch(`https://mindicador.cl/api/${moneda}`)
+        const res = await data.json()
+        return res
+    } catch (e) {
+    const errorSpan = document.querySelector("#errorspan")
+    errorSpan.innerHTML = `Algo salió mal! Error: ${e.message}`
+}}
 
-const crearGrafico = async(series) => {
-    const data = series.map((serie)=> serie.valor)
-    const fechas = series.map((serie)=> serie.fecha)
-
+const crearGrafico = async (series) => {
+    const data = series.map((serie) => serie.valor)
+    const fechas = series.map((serie) => serie.fecha)
+    // const date = new Date(fechas)
+    // let nuevaFecha = date.toLocaleDateString()
     const ctx = document.getElementById('myChart');
 
     new Chart(ctx, {
@@ -21,7 +25,7 @@ const crearGrafico = async(series) => {
                 data: data.slice(0, 10).reverse(),
                 borderWidth: 1
             },
-          ],
+            ],
         },
     });
 }
@@ -30,14 +34,14 @@ formulario.addEventListener("submit", async (event) => {
     event.preventDefault()
     let grafico = Chart.getChart("myChart")
 
-    if(grafico !== undefined){
+    if (grafico !== undefined) {
         grafico.destroy()
     }
 
     const clp = document.querySelector("#clp").value
     const moneda = document.querySelector("#moneda").value
     const result = await obtenerData(moneda)
-    
+
     let calculo = clp / result.serie[0].valor
 
     let calculoSinDecimales = Number(calculo).toFixed(2)
